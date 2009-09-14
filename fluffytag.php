fbc39d3b4dfc355505f16418f8520c35c65b305f
eighty4 <USER@DOMAIN>
2009-09-14 15:17:41 +0000

    adding 0.6 branch and updating trunk to 0.7
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/fluffytag/trunk@2298 f4a90942-1786-4aa1-b092-57c31935ddc3

--- fluffytag.php
+++ fluffytag.php
@@ -1,6 +1,7 @@
-<ul id="tag-cloud">
+<h3 id="tagcloud"><?php _e('Tag cloud'); ?></h3>
+<ul id="fluffytag">
 <?php
-foreach( $theme->fluffytag as $tag ) {
+foreach( $theme->fluffy as $tag ) {
 	echo '<li class="step-' . $tag['step'] . '"><a href="' . 
 	URL::get( 'display_entries_by_tag', array ( 'tag' => $tag['slug'] ), false ) . '" rel="tag" title="' . $tag['tag'] .'">' . $tag['tag'] . "</a></li>\n";  
 }
